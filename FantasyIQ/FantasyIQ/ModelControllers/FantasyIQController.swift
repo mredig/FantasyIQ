c5920c4945db75108738fedc4f9d381a9e3d6b9e
luqmaan <luq.j.khan@gmail.com>
2019-09-24 14:42:33 -0500

    Add logic fo rdepth chart ranking per position and calculating trade value for team

--- FantasyIQ/FantasyIQ/ModelControllers/FantasyIQController.swift
+++ FantasyIQ/FantasyIQ/ModelControllers/FantasyIQController.swift
@@ -44,7 +44,7 @@ class FantasyIQController {
     }
     
     //in the arguments you have to take the name of the player, in the body before the request find the corresponding player id and use in url
-    func fetchProjectedSeasonStats(playerName: String,completion: @escaping (Player?, Error?) -> Void = {_,_ in}) {
+    func fetchPlayerAndProjectedSeasonStats(playerName: String,completion: @escaping (Player?, Error?) -> Void = {_,_ in}) {
         //projections/json/PlayerSeasonProjectionStatsByPlayerID/2019REG/14986
          let player = allFantasyPlayers.filter{$0.Name == playerName}.first
         guard let id = player?.PlayerID else {return}
@@ -65,6 +65,7 @@ class FantasyIQController {
             do {
                 let receivedData = try JSONDecoder().decode(Player.self, from: data)
                 self.projectedSeasonStats = receivedData
+                self.team.append(receivedData)
                 completion(self.projectedSeasonStats, nil)
             } catch {
                 NSLog("Error decoding projected season stats: \(error)")
@@ -86,4 +87,61 @@ class FantasyIQController {
         return projectedFantasyPoints
     }
     
+    func calculateDepthChartRankingForQB(team: [Player]) -> String {
+        let allQBs = team.filter{$0.Position!.contains("QB")}
+        for qb in allQBs {
+            guard let seasonFantasyPoints = qb.FantasyPoints else { NSLog("Error getting qb depth ranking");return ""}
+            if seasonFantasyPoints >= 300.0 && allQBs.count >= 2 {
+                return "Strong"
+            } else if seasonFantasyPoints >= 200.0 && seasonFantasyPoints < 300 && allQBs.count < 2 {
+                return "Medium"
+            } else {
+                return "Weak"
+            }
+        }
+        return ""
+    }
+    
+    
+       func calculateDepthChartRankingForWR(team: [Player]) -> String {
+           let allwrs = team.filter{$0.Position!.contains("WR")}
+           for wr in allwrs {
+               guard let seasonFantasyPoints = wr.FantasyPoints else { NSLog("Error getting WR depth ranking");return ""}
+               if seasonFantasyPoints >= 250.0 && allwrs.count >= 2 {
+                   return "Strong"
+               } else if seasonFantasyPoints >= 150.0 && seasonFantasyPoints < 300 && allwrs.count < 2 && allwrs.count > 0 {
+                   return "Medium"
+               } else {
+                   return "Weak"
+               }
+           }
+           return ""
+       }
+       
+    func calculateDepthChartRankingForRB(team: [Player]) -> String {
+        let allrbs = team.filter{$0.Position!.contains("RB")}
+        for rb in allrbs {
+            guard let seasonFantasyPoints = rb.FantasyPoints else { NSLog("Error getting RB depth ranking");return ""}
+            if seasonFantasyPoints >= 250.0 && allrbs.count >= 2 {
+                return "Strong"
+            } else if seasonFantasyPoints >= 150.0 && seasonFantasyPoints < 300 && allrbs.count < 2 && allrbs.count > 0 {
+                return "Medium"
+            } else {
+                return "Weak"
+            }
+        }
+        return ""
+    }
+    
+    func calculateTradeCombinations(team: [Player]) -> String {
+       
+        if calculateDepthChartRankingForQB(team: team) == "Weak" {
+            return "Trade one high value player or two medium valued players for a QB"
+        } else if  calculateDepthChartRankingForRB(team: team) == "Weak" {
+            return "Trade one high value player or two medium valued players for a RB"
+        } else if calculateDepthChartRankingForWR(team: team) == "Weak" {
+           return "Trade one high value player or two medium valued players for a WR"
+        }
+       return ""
+    }
 }
